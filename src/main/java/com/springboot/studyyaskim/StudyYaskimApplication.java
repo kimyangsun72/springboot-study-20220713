d2a588b667ff25f95ddfa0835d6499aa5ce4ea1f
DESKTOP-72VS647\KR <yaskim@krs.co.kr>
2022-07-13 19:13:08 +0900

    test init

---
+++ src/main/java/com/springboot/studyyaskim/StudyYaskimApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.studyyaskim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyYaskimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyYaskimApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/studyyaskim/StudyYaskimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.studyyaskim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyYaskimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
